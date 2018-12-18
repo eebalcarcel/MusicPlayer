8bd5ac0e8634ac392c90685d99c607bde3ca4e41
Esteban <esttueb@gmail.com>
2018-12-18 01:10:31 -0300

    Add splash screen

---
+++ app/src/main/java/com/eeb/musicplayer/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.eeb.musicplayer;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME = 500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setStatusBarColor(0x00000000);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
