e8a542b488617b13bc0bb8497ac2f4aebce2ca6a
Esteban <esttueb@gmail.com>
2018-12-22 19:34:35 -0300

    Add elapsed time and track duration to bottomBar

--- app/src/main/java/com/eeb/musicplayer/Track.java
+++ app/src/main/java/com/eeb/musicplayer/Track.java
@@ -49,13 +49,13 @@ class Track extends File{
      *
      * @return String with 'hh:mm:ss' or 'mm:ss' if it doesn't have hours
      */
-    public String getTime(){
+    public static String getFormattedDuration(int milliseconds){
         String timeWithoutHours = String.format(Locale.US, "%02d:%02d",
-                TimeUnit.MILLISECONDS.toMinutes(duration),
-                TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
-        String time = String.format(Locale.US,"%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(duration),
-                TimeUnit.MILLISECONDS.toMinutes(duration) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration)),
-                TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
-        return TimeUnit.MILLISECONDS.toHours(duration)==0?timeWithoutHours:time;
+                TimeUnit.MILLISECONDS.toMinutes(milliseconds),
+                TimeUnit.MILLISECONDS.toSeconds(milliseconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliseconds)));
+        String time = String.format(Locale.US,"%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(milliseconds),
+                TimeUnit.MILLISECONDS.toMinutes(milliseconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliseconds)),
+                TimeUnit.MILLISECONDS.toSeconds(milliseconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliseconds)));
+        return TimeUnit.MILLISECONDS.toHours(milliseconds)==0?timeWithoutHours:time;
     }
 }
